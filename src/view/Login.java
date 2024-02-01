a92847ae5051c7fd0e3e96dc65619b6c3cfb2069
LAURA LOPES DOS SANTOS <laura.lsantos1@senacsp.edu.br>
2024-02-01 11:48:18 -0300

    Criação da janela de login, download do driver JDBC

---
+++ src/view/Login.java
@@ -0,0 +1,98 @@
+package view;
+
+import javax.swing.JDialog;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Rectangle;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.Cursor;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import java.awt.ComponentOrientation;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+
+public class Login extends JDialog {
+	private JTextField inputLogin;
+	private JPasswordField inputSenha;
+	private JLabel tituloLogin;
+	private JButton btnLogin;
+	private JLabel imgDatabase;
+	
+	public Login() {
+		setTitle("Login");
+		setBounds(new Rectangle(600, 250, 430, 301));
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setResizable(false);
+		setModal(true);
+		getContentPane().setBackground(new Color(255, 255, 255));
+		setBackground(new Color(255, 255, 255));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/img/logo.png")));
+		getContentPane().setLayout(null);
+		
+		JLabel txtLogin = new JLabel("Login:");
+		txtLogin.setFont(new Font("Yu Gothic UI", Font.PLAIN, 13));
+		txtLogin.setBounds(84, 96, 338, 29);
+		getContentPane().add(txtLogin);
+		
+		JLabel txtSenha = new JLabel("Senha:");
+		txtSenha.setFont(new Font("Yu Gothic UI", Font.PLAIN, 13));
+		txtSenha.setBounds(84, 145, 338, 29);
+		getContentPane().add(txtSenha);
+		
+		inputLogin = new JTextField();
+		inputLogin.setBounds(135, 101, 183, 20);
+		getContentPane().add(inputLogin);
+		inputLogin.setColumns(10);
+		
+		inputSenha = new JPasswordField();
+		inputSenha.setBounds(135, 150, 183, 20);
+		getContentPane().add(inputSenha);
+		
+		tituloLogin = new JLabel("Acessar conta");
+		tituloLogin.setFont(new Font("Yu Gothic UI", Font.BOLD, 17));
+		tituloLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		tituloLogin.setBounds(0, 27, 432, 29);
+		getContentPane().add(tituloLogin);
+		
+		btnLogin = new JButton("Entrar");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnLogin.setBounds(162, 208, 89, 23);
+		getContentPane().add(btnLogin);
+		
+		imgDatabase = new JLabel("");
+		imgDatabase.setIcon(new ImageIcon(Login.class.getResource("/img/databaseOff.png")));
+		imgDatabase.setBounds(20, 196, 54, 55);
+		getContentPane().add(imgDatabase);
+	}
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login dialog = new Login();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});	
+	}
+}
+
--- src/view/Sobre.java
+++ src/view/Sobre.java
@@ -1,18 +1,20 @@
 package view;
 
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import javax.swing.SwingConstants;
-import java.awt.Font;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
+
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Sobre extends JDialog {
 	
 	public Sobre() {
+		setBounds(new Rectangle(600, 250, 479, 339));
 		setResizable(false);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setModal(true);
@@ -22,7 +24,7 @@ public class Sobre extends JDialog {
 		
 		JLabel titulo = new JLabel("Sobre o software");
 		titulo.setHorizontalAlignment(SwingConstants.CENTER);
-		titulo.setBounds(0, 31, 493, 24);
+		titulo.setBounds(0, 31, 464, 24);
 		titulo.setBackground(new Color(0, 0, 0));
 		titulo.setForeground(new Color(0, 0, 0));
 		titulo.setFont(new Font("Yu Gothic", Font.BOLD, 14));
@@ -31,31 +33,31 @@ public class Sobre extends JDialog {
 		JLabel descricao1 = new JLabel("possibilitar o gerenciamento de reserva de salas em um espaço colaborativo.\r\n");
 		descricao1.setHorizontalAlignment(SwingConstants.CENTER);
 		descricao1.setFont(new Font("Yu Gothic UI", Font.PLAIN, 12));
-		descricao1.setBounds(0, 127, 493, 33);
+		descricao1.setBounds(0, 127, 461, 33);
 		getContentPane().add(descricao1);
 		
 		JLabel descricao2 = new JLabel("O software CoWorking trata-se de um protótipo cujo o objetivo é");
 		descricao2.setHorizontalAlignment(SwingConstants.CENTER);
 		descricao2.setFont(new Font("Yu Gothic UI", Font.PLAIN, 12));
-		descricao2.setBounds(0, 98, 493, 33);
+		descricao2.setBounds(0, 98, 461, 33);
 		getContentPane().add(descricao2);
 		
 		JLabel versao = new JLabel("Versão 1.0.0\r\n");
 		versao.setHorizontalAlignment(SwingConstants.CENTER);
 		versao.setFont(new Font("Yu Gothic UI", Font.PLAIN, 12));
-		versao.setBounds(0, 212, 493, 33);
+		versao.setBounds(0, 212, 461, 33);
 		getContentPane().add(versao);
 		
 		JLabel atualizacao = new JLabel("Última atualização: 31/01/2024");
 		atualizacao.setHorizontalAlignment(SwingConstants.CENTER);
 		atualizacao.setFont(new Font("Yu Gothic UI", Font.PLAIN, 12));
-		atualizacao.setBounds(0, 237, 493, 33);
+		atualizacao.setBounds(0, 237, 464, 33);
 		getContentPane().add(atualizacao);
 		
 		JLabel imgMIT = new JLabel("");
 		imgMIT.setHorizontalAlignment(SwingConstants.CENTER);
 		imgMIT.setIcon(new ImageIcon(Sobre.class.getResource("/img/mitLicense.png")));
-		imgMIT.setBounds(430, 245, 53, 48);
+		imgMIT.setBounds(404, 245, 53, 48);
 		getContentPane().add(imgMIT);
 	}
 	
